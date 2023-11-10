322f7456531548a9e7933d999fb30505778e9624
Aravind Karnam <aravind.karanam@gmail.com>
2023-11-10 15:47:08 +0530

    fixed typo in pdf file name

--- src/pages/index.js
+++ src/pages/index.js
@@ -358,7 +358,7 @@ const BlogIndex = ({ data, location }) => {
                     </ol>
                     <a
                       className="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full"
-                      href="/aravind_CV.pdf"
+                      href="/Aravind_CV.pdf"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
